bf30cfcbafd5a574e67b6cca2a867622b30b5563
saket kumar <saketbittu012@gmail.com>
2022-06-24 21:34:32 +0530

    todo for Amar

---
+++ app/Constant/seo.js
@@ -0,0 +1,2 @@
+// Todo: array of object
+
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -32,6 +32,7 @@ import { changeUsername } from './actions';
 import { makeSelectUsername } from './selectors';
 import reducer from './reducer';
 import saga from './saga';
+import './index.css';
 
 const key = 'home';
 
@@ -49,7 +50,6 @@ export function HomePage({
   useEffect(() => {
     // When initial state username is not null, submit the form to load repos
     if (username && username.trim().length > 0) onSubmitForm();
-    // todo: pop-up launch after 40 sec [write logic here]
   }, []);
 
   const reposListProps = {
@@ -62,18 +62,12 @@ export function HomePage({
     <article>
       <Helmet>
         <title>Home Page</title>
-        {
-          // todo: title and meta can be added like this
-        }
         <meta
           name="description"
           content="A React.js Boilerplate application homepage"
         />
       </Helmet>
-      <div>
-        {
-          // todo: Video in background : image in backgrond for page below 
-        }
+      <div className="main-div">
         <CenteredSection>
           <H2>
             <FormattedMessage {...messages.startProjectHeader} />
@@ -103,6 +97,7 @@ export function HomePage({
           </Form>
           <ReposList {...reposListProps} />
         </Section>
+        
       </div>
     </article>
   );
