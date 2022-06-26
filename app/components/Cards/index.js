410883dd75ecef30a4f7376c2021039c729a7c23
Akshay Kumar <88998823+Akshay80837@users.noreply.github.com>
2022-06-26 17:00:13 +0530

    conflict  resolve

---
+++ app/components/Cards/index.js
@@ -0,0 +1,18 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Card({ propsData }) {
+  return <div />;
+}
+
+Card.propTypes = {
+  propsData: PropTypes.string,
+};
+
+export default Card;
