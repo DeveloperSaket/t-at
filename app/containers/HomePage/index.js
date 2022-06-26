b02cafd7a5537db1fc4160140e0b1b3ecacefa90
saket kumar <saketbittu012@gmail.com>
2022-06-26 16:58:31 +0530

    code clean-up

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,8 +6,9 @@
 
 import React, { useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
@@ -19,14 +20,14 @@ import {
   makeSelectLoading,
   makeSelectError,
 } from 'containers/App/selectors';
-import H2 from 'components/H2';
-import ReposList from 'components/ReposList';
-import AtPrefix from './AtPrefix';
-import CenteredSection from './CenteredSection';
-import Form from './Form';
-import Input from './Input';
-import Section from './Section';
-import messages from './messages';
+// import H2 from 'components/H2';
+// import ReposList from 'components/ReposList';
+// import AtPrefix from './AtPrefix';
+// import CenteredSection from './CenteredSection';
+// import Form from './Form';
+// import Input from './Input';
+// import Section from './Section';
+// import messages from './messages';
 import { loadRepos } from '../App/actions';
 import { changeUsername } from './actions';
 import { makeSelectUsername } from './selectors';
@@ -42,7 +43,7 @@ export function HomePage({
   error,
   repos,
   onSubmitForm,
-  onChangeUsername,
+  // onChangeUsername,
 }) {
   useInjectReducer({ key, reducer });
   useInjectSaga({ key, saga });
@@ -53,11 +54,15 @@ export function HomePage({
     // todo: pop-up launch after 40 sec [write logic here]
   }, []);
 
-  const reposListProps = {
-    loading,
-    error,
-    repos,
-  };
+  // const reposListProps = {
+  //   loading,
+  //   error,
+  //   repos,
+  // };
+
+  const ContentWrapper = styled.div`
+    padding: 0 16px;
+  `;
 
   return (
     <article>
@@ -71,26 +76,28 @@ export function HomePage({
           content="A React.js Boilerplate application homepage"
         />
       </Helmet>
-      <div>
-        {
-          // todo: Video in background : image in backgrond for page below 
-        }
+      <ContentWrapper>
         <div>
-          {homepage.sec1.cards.map(v=><div>{v.text}</div>)}
-        </div>
-        <div style={{marginTop: '15px'}}>
-          {homepage.sec2.cards.map(v=><div>{v.text}</div>)}
-        </div>
-        <div style={{marginTop: '15px'}}>
-          {homepage.sec3.cards.map(v=><div>{v.text}</div>)}
-        </div>
-        <div style={{marginTop: '15px'}}>
-          {homepage.sec4.cards.map(v=><div>{v.text} - {v.days} - {v.oldCharges} - {v.charges}</div>)}
-        </div>
-        <div style={{marginTop: '15px'}}>
-          {homepage.sec5.cards.map(v=><div>{v.text}</div>)}
-        </div>
-        <CenteredSection>
+          {
+            // todo: Video in background : image in backgrond for page below 
+          }
+          <div>
+            {homepage.sec1.cards.map(v => <div>{v.text}</div>)}
+          </div>
+          <div style={{ marginTop: '15px' }}>
+            {homepage.sec2.cards.map(v => <div>{v.text}</div>)}
+          </div>
+          <div style={{ marginTop: '15px' }}>
+            {homepage.sec3.cards.map(v => <div>{v.text}</div>)}
+          </div>
+          <div style={{ marginTop: '15px' }}>
+            {homepage.sec4.cards.map(v => <div>{v.text} - {v.days} - {v.oldCharges} - {v.charges}</div>)}
+          </div>
+          <div style={{ marginTop: '15px' }}>
+            {homepage.sec5.cards.map(v => <div>{v.text}</div>)}
+          </div>
+
+          {/* <CenteredSection>
           <H2>
             <FormattedMessage {...messages.startProjectHeader} />
           </H2>
@@ -118,8 +125,9 @@ export function HomePage({
             </label>
           </Form>
           <ReposList {...reposListProps} />
-        </Section>
-      </div>
+        </Section> */}
+        </div>
+      </ContentWrapper>
     </article>
   );
 }
